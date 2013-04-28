b453cf4ecca2a84ad111f0f4249240cbc1fce83a
Dean Marano <dmarano@groupon.com>
2013-04-27 19:19:44 -0500

    Sign up with Facebook.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,13 +14,34 @@ class SessionsController < ApplicationController
   end
 
   def oauth
-    Rails.logger.info request.env['omniauth.auth']
-
     token = request.env['omniauth.auth'].credentials.token
     expiration = Time.at(request.env['omniauth.auth'].credentials.expires_at)
 
-    current_user.update_attributes(:facebook_token => token, :facebook_token_expiration => expiration)
-    redirect_to :root
+    if current_user
+      Rails.logger.info 'current user found'
+      current_user.update_attributes(:facebook_token => token, :facebook_token_expiration => expiration)
+      session[:user_id] = user.id # create new session, find the user.id and hold in the new session cookie as :user_id...
+      redirect_to :root
+    else
+      email = request.env['omniauth.auth'].info.email
+      if user = User.find_by_email(email)
+        Rails.logger.info 'existing user found'
+        session[:user_id] = user.id # create new session, find the user.id and hold in the new session cookie as :user_id...
+        redirect_to :root
+      else
+        Rails.logger.info 'create user from facebook'
+        user = User.new(data_from_facebook(request.env['omniauth.auth']))
+        if user.save
+          Rails.logger.info 'user created from facebook'
+          session[:user_id] = user.id # create new session, find the user.id and hold in the new session cookie as :user_id...
+          redirect_to example_scorecards_path
+        else
+          Rails.logger.info "failed to create user from facebook: #{user.errors.full_messages}"
+          redirect_to :root
+        end
+      end
+    end
+
   end
 
   def sign_up
@@ -39,9 +60,26 @@ class SessionsController < ApplicationController
   end
 
   def reset_password
-
+    UserMailer.reset_password(User.find_by_email(params[:email])).deliver
   end
 
   def change_password
+  end
+
+  private
+
+  def data_from_facebook(omniauth_data)
+    token = omniauth_data.credentials.token
+    expiration = Time.at(omniauth_data.credentials.expires_at)
+    info = omniauth_data.info
+    username = info.name.downcase.gsub(' ', '-')
+    {
+      :username => username,
+      :first_name => info.first_name,
+      :last_name => info.last_name,
+      :email => info.email,
+      :password => rand(36**16).to_s(36),
+      :facebook_token => token,
+      :facebook_token_expiration => expiration}
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,16 @@ class User < ActiveRecord::Base
   has_many :roles
   has_many :animals, :through => :roles
 
-  attr_accessible :email, :first_name, :last_name, :username, :password, :password_confirmation, :password_digest, :phone, :facebook_token, :facebook_token_expiration
+  attr_accessible :email,
+    :first_name,
+    :last_name,
+    :username,
+    :password,
+    :password_confirmation,
+    :password_digest,
+    :phone,
+    :facebook_token,
+    :facebook_token_expiration
 
   has_secure_password
   validates_presence_of :email, :username
